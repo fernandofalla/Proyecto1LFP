997befc289adac40781b92d634a4b2846c1bf626
Luis Falla <2992202650101@ingenieria.usac.edu.gt>
2021-03-15 00:11:16 -0600

    Respaldo

--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
 import os
+from Token import Token
+from Token import Valor
+from Token import Error
+
+v = Valor()
+
+
 
 def Menu():
 
@@ -10,6 +17,7 @@ def Menu():
     print("     |   Carné: 201700700                     |")
     print("     ------------------------------------------")
     print()
+    print("0. Mostrar")
     print("1. Cargar menú")
     print("2. Cargar orden")
     print("3. Generar menú")
@@ -21,7 +29,10 @@ def Menu():
     
     opcion = input(">> ")
 
-    if opcion == "1":
+    if opcion == "0":
+        mostrar()
+        Menu()
+    elif opcion == "1":
         Cargar_Menu()
         Menu()
     elif opcion == "2":
@@ -42,9 +53,22 @@ def Menu():
         print("Opcion incorrecta")
         Menu()
 
+def mostrar():
+    AFD(v.entrada)
+    print("Lista de Tokens")
+    print()
+    Mostrar_Token()
+    print("Lista de Errores")
+    print()
+    Mostrar_Error()
 
 def Cargar_Menu():
-    pass
+    archivo = input("Ingrese archivo: ")
+
+    entrada = open(archivo, "r")
+
+    for i in entrada.read():
+        v.entrada += i
 
 def Cargar_Orden():
     pass
@@ -58,12 +82,180 @@ def Generar_Factura():
 def Generar_Arbol():
     pass
 
-def AFD():
-    pass
+def AFD(entrada):
+    v.entrada = v.entrada + "#"
 
-def Guardar_Token():
-    pass
+    for elemento in v.entrada:
+        if v.estado == 0:
+            if elemento == " ":
+                v.columna += 1
+                v.estado = 0
+            if elemento == "":
+                v.fila += 1
+            elif elemento == '\n':
+                v.fila += 1
+                v.columna = 1
+                v.estado = 0
+            elif elemento == "\t":
+                v.columna += 1
+                v.estado = 0
+            elif elemento == "=":
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("igual")
+            elif elemento == ":":
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("dos_puntos")
+            elif elemento == ";":
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("punto_coma")
+            elif elemento == "[":
+                v.cadena += elemento
+                Guardar_Token("corch_ab")
+                v.columna += 1
+                v.estado = 4
+            elif elemento == "]":
+                v.cadena += elemento
+                Guardar_Token("corch_ce")
+                v.columna += 1
+            elif elemento.isalpha():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 1
+            elif elemento.isdigit():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 5
+            elif elemento == "'":
+                v.columna += 1
+                v.estado = 2
+        elif v.estado == 1:
+            if elemento.isalpha():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 1
+            elif elemento == " ":
+                v.columna += 1
+                v.estado = 1
+            elif elemento == "=":
+                Guardar_Token("Restaurante")
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("igual")
+        elif v.estado == 2:
+            if elemento.isalpha():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 2
+            elif elemento.isdigit():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 2
+            elif elemento == "_":
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 2
+            elif elemento == " ":
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 2
+            elif  elemento == "#":
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 2
+            elif elemento == "'":
+                v.columna += 1
+                v.estado = 3
+        elif v.estado == 3:
+            if " " in  v.cadena:
+                Guardar_Token("nombre_res")
+            else:
+                Guardar_Token("nombre_sec")
+        elif v.estado == 4:
+            if elemento.isalpha():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 4    
+            elif elemento.isdigit():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 4
+            elif elemento == "_":
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 4
+            elif elemento == ";":
+                Guardar_Token("id")
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("punto_coma")
+            elif elemento == " ":
+                if v.cadena == "":
+                    v.estado = 4
+                else:
+                    v.columna += 1
+                    v.cadena += elemento
+                    Guardar_Error("id_inv")
+        elif v.estado == 5:
+            if elemento.isdigit():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 5
+            elif elemento == " ":
+                v.columna += 1
+                v.estado = 5
+            elif elemento == ";":
+                Guardar_Token("numero")
+                v.columna += 1
+                v.cadena += elemento
+                Guardar_Token("punto_coma")
+            elif elemento == ".":
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 6
+        elif v.estado == 6:
+            if elemento.isdigit():
+                v.columna += 1
+                v.cadena += elemento
+                v.estado = 6
+            elif elemento == ";":
+                if v.cadena[-1] == ".":
+                    Guardar_Token("numero")
+                    v.columna += 1
+                    v.cadena += elemento
+                    Guardar_Token("punto_coma")
+                else:
+                    Guardar_Token("decimal")
+                    v.columna += 1
+                    v.cadena += elemento
+                    Guardar_Token("punto_coma")
+       
 
-def Guardar_Error():
-    pass
 
+def Guardar_Token(token):
+    token = Token(token, v.cadena, v.fila, v.columna)
+
+    v.Lista_Token.append(token.Retornartoken())
+
+    v.cadena = ""
+    v.estado = 0
+
+def Guardar_Error(tipo):
+    error = Error(tipo, v.fila, v.columna, v.cadena)
+
+    v.Lista_Error.append(error.Retornar_Error())
+
+    v.cadena = ""
+    v.estado = 0
+
+def Mostrar_Token():
+    for i in v.Lista_Token:
+        print(i)
+
+def Mostrar_Error():
+    for i in v.Lista_Error:
+        print(i)
+
+Menu()
---
+++ Token.py
@@ -0,0 +1,100 @@
+class Token:
+
+    def __init__(self, token, lexema, fila, columna):
+        self.token = token
+        self.lexema = lexema
+        self.fila = fila
+        self.columna = columna
+
+    def mostrar(self):
+        print(self.token, self.lexema, self.fila, self.columna)
+
+    def Retornartoken(self):
+        if self.token == "Restaurante":
+            return "RESERVADA_RES", self.lexema, self.fila, self.columna
+        elif self.token == "igual":
+            return "SIMB_IGUAL", self.lexema, self.fila, self.columna
+        elif self.token == "id":
+            return "IDENTIFICADOR", self.lexema, self.fila, self.columna
+        elif self.token == "nombre_res":
+            return "NOMBRE_RES", self.lexema, self.fila, self.columna
+        elif self.token == "nombre_sec":
+            return "NOMBRE_SEC", self.lexema, self.fila, self.columna
+        elif self.token == "dos_puntos":
+            return "DOS_PUNTOS", self.lexema, self.fila, self.columna
+        elif self.token == "corch_ab":
+            return "CORCHETE_AB", self.lexema, self.fila, self.columna
+        elif self.token == "punto_coma":
+            return "PUNTO_COMA", self.lexema, self.fila, self.columna
+        elif self.token == "punto":
+            return "SIMB_PUNTO", self.lexema, self.fila, self.columna
+        elif self.token == "corch_ce":
+            return "CORCHETE_CE", self.lexema, self.fila, self.columna
+        elif self.token == "numero":
+            return "NUMERO_ENTERO", self.lexema, self.fila, self.columna
+        elif self.token == "decimal":
+            return "NUMERO_DECIMAL", self.lexema, self.fila, self.columna
+        else:
+            return "SIMBOLO DESCONOCIDO", self.lexema, self.fila, self.columna
+        '''
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        elif self.token == "":
+            return "", self.lexema, self.fila, self.columna
+        '''
+       
+class Error:
+
+    def __init__(self, tipo, fila, columna, caracter):
+        self.tipo = tipo
+        self.fila = fila
+        self.columna = columna
+        self.caracter = caracter
+    
+    def Mostrar_Error(self):
+        print(self.fila, self.columna, self.caracter)
+    
+    def Retornar_Error(self):
+        if self.tipo == "id_inv":
+            return self.fila, self.columna, self.caracter, "Identificador no valido"
+       
+class Valor:
+
+    def __init__(self):
+        self.entrada = ""
+        self.cadena = ""
+        self.cadenaAux = ""
+        self.estado = 0
+        self.Lista_Token = []
+        self.Lista_Error = []
+        self.aux = ""
+        self.fila = 1
+        self.columna = 1
+        self.token = []
+        self.index = 0
